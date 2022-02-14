4f4575169475274b5dde3d2d8502192d9a049d04
tianrking <tian.r.king@gmail.com>
2022-02-14 09:13:13 +0800

    ad_weixin_qq_com.py

--- app/a.py
+++ app/a.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+from bs4 import BeautifulSoup
+from sqlalchemy import null
+
 
 options = Options()
 options.headless = True
@@ -19,11 +22,28 @@ driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
 # email.send_keys(config.email)
 # password.send_keys(config.password)
 # password.submit()
- 
-url = "https://ad.weixin.qq.com/guide/" + "106"
-driver.get(url)
-print(driver.title)
-driver.quit()
+for i in range(106,3000):
+    url = "https://ad.weixin.qq.com/guide/" + str(i)
+    driver.get(url)
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    # data = driver.find_element("//meta[@name='description']")
+    # print(soup.find(name='meta')) # <meta>
+    f = open('log.txt','a')
+    if len(soup.find_all(attrs={'class': 'guide__title'})):
+        
+        f.write(str(soup.find_all(attrs={'class': 'guide__title'})))
+        f.write(",")
+        f.write(url)
+        f.write('\n')
+        # f.write(soup.find_all(attrs={'class': 'guide__title'})+","+list(url)+'\n')
+        #f.close()
+    f.close() 
+    # print(soup.find_all(attrs={'class': 'guide__title'}),url)
+
+    # print(soup.find_all('div'))
+    # print(soup)
+    # driver.quit()
 
 # print(driver.title)
 # driver.quit()
