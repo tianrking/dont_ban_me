8a51a785a72836e510aafa9cb3c068eae207ceb1
tianrking <tian.r.king@gmail.com>
2022-02-15 17:08:38 +0800

    add tencent ad help

--- app/2腾讯广告帮助中心/a.py
+++ app/2腾讯广告帮助中心/a.py
@@ -1,13 +1,61 @@
 
-import requests,json
+from multiprocessing.connection import answer_challenge
+from posixpath import dirname
+import requests
+import json
 from tqdm import tqdm # 进度条
 import requests
 from bs4 import BeautifulSoup
+import _thread 
+import pandas as pd
 
-url = 'https://e.qq.com/portal/helper/contents?post_id=1712'
-res = requests.get(url,cookies = {'nothing':'1'}) # 
-Soup = BeautifulSoup(res.text,'html.parser')
-JS = json.loads(res.text)
-print(JS)
-# print(Soup.title)
-# print(Soup.title.text)
+
+def request_api(T_name,begin,end):
+    df = pd.DataFrame()
+    for i in tqdm(range(begin,end)):
+        url = 'https://e.qq.com/portal/helper/contents?post_id=%s' % str(i) #1712
+        res = requests.get(url,cookies = {'nothing':'1'}) # 
+        Soup = BeautifulSoup(res.text,'html.parser')
+        JS = json.loads(res.text)
+
+        if JS['code']!=0:
+            continue
+        if JS['code']==0:
+            question = JS['data']['title'] # 问题
+            answer = JS['data']['content'] # 文字答案
+            dir = 'data_all_in_one/'
+            dirname = dir + "QA"+'.txt'
+            write_data = question+',' + answer + "\n"
+            # f = open(dirname,'a')
+            # f.write(write_data)
+            # f.close()
+            df = pd.DataFrame(data=[
+                [question,answer]],
+                columns = ['Q','A'],
+                )
+            # df = 
+            df.to_csv('data_all_in_one/QA.csv', mode='a', header=False)
+            # print(name,answer)
+            
+        # print(T_name)
+        # print(i)
+
+
+try:
+    begin = 1000 
+    end = 5000
+    sum = end - begin
+    step = 4
+    time = int(sum / step)
+    _thread.start_new_thread(request_api, ("Thread-1", 0 , time) )
+    _thread.start_new_thread(request_api, ("Thread-2", time , time*2) )
+    _thread.start_new_thread(request_api, ("Thread-3", time*2 , time*3) )
+    _thread.start_new_thread(request_api, ("Thread-4", time*3,  time*4) )
+    # _thread.start_new_thread(download, ("Thread-5", start , k , 3) )
+except:
+   print ("Error: 无法启动线程")
+
+
+while 1:
+    pass
+
