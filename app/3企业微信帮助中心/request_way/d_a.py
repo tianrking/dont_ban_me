422af77e8d164cad76694cc88af3b61b2092e36d
tianrking <tian.r.king@gmail.com>
2022-02-15 12:59:34 +0800

    add 微信企业帮助中心

---
+++ app/3企业微信帮助中心/request_way/d_a.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+pd.set_option('display.max_columns',None)
+#显示所有行
+# pd.set_option('display.max_rows',None)
+#设置数据的显示长度，默认为50
+
+pd.set_option('max_colwidth',200)
+df = pd.read_csv('./requests_url_thread.txt',header=None,names=['KEY','URL'])
+# print(df.head())
+
+
+## df.drop df[~df]
+awesome = df[df['KEY']!='帮助中心-企业微信']
+print(awesome)
+
+awesome.to_csv('awesome_link.csv')
