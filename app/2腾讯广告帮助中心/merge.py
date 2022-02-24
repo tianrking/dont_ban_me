1189134a6455034679e8b3fc47f9404207d83222
tianrking <tian.r.king@gmail.com>
2022-02-24 11:43:28 +0800

    update 2 data but I am not sure whether it is safe

--- app/2腾讯广告帮助中心/merge.py
+++ app/2腾讯广告帮助中心/merge.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-c_dir = 'data_all_in_one'
+c_dir = 'data_all_in_one_with_url'
 files = os.listdir(c_dir)
 
 print(files)
@@ -10,13 +10,16 @@ print(files)
 
 df = pd.DataFrame()
 for csv in files:
-    df_temp = pd.read_csv(c_dir+"/"+csv)
-    # df = pd.concat([df,df_temp])
-    # df.append(df_temp)
-    
-    df_temp.to_csv('final.csv',index=False, header=False, mode='a+')
-    df = pd.read_csv('final.csv')
-    print(df.shape)
+    try:
+        df_temp = pd.read_csv(c_dir+"/"+csv)
+        # df = pd.concat([df,df_temp])
+        # df.append(df_temp)
+        
+        df_temp.to_csv('final_with_url.csv',index=False, header=False, mode='a+')
+        df = pd.read_csv('final_with_url.csv')
+        print(df.shape)
+    except:
+        print(c_dir+"/"+csv+"  is wrong")
     
 
 # df.to_csv('final.csv')
